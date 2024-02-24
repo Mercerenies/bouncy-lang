c7bc3a78181f6558b89825b2411f638ebee9d403
Silvio Mayolo <mercerenies@comcast.net>
2024-02-24 17:04:09 -0500

    More interpreter values

--- bouncy.rb
+++ bouncy.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+require_relative './point'
 require_relative './instruction'
 require_relative './instruction/mode'
 require_relative './language/grid'
+require_relative './language/interpreter'
 
 example_program = <<~PROGRAM.chomp
   $...@
@@ -10,4 +12,8 @@ example_program = <<~PROGRAM.chomp
   .....
 PROGRAM
 grid = Language::Grid.load_from_string(example_program)
+if grid.empty? # Prevents division by zero errors when assigning instruction pointer
+  grid = Language::Grid.new
+  grid[Point[0, 0]] = ' '
+end
 puts grid
--- language/interpreter.rb
+++ language/interpreter.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
 require_relative '../point'
+require_relative '../instruction/mode'
 require_relative './pointer'
+require_relative './data_array'
 
 module Language
   class Interpreter
-    attr_reader :grid, :pointer
+    attr_reader :grid, :instruction_pointer
+
+    attr_accessor :register_value, :memory_pointer
 
     def initialize(grid)
       @grid = grid
-      @pointer = Pointer.new(grid.find_starting_point)
+      @instruction_pointer = Pointer.new(grid.bounds, grid.find_starting_point)
+      @data_arrays = Mode.each.map { |mode| [mode, DataArray.new] }.to_h
+      @register_value = 0
+      @memory_pointer = 0
+    end
+
+    def data_array(mode)
+      @data_arrays.fetch(mode)
     end
   end
 end
