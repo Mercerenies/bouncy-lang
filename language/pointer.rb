b3d3f5ce3b24326c2ffbbc06dd54588ad9411777
Silvio Mayolo <mercerenies@comcast.net>
2024-02-24 17:04:01 -0500

    Smarter pointer class

--- language/pointer.rb
+++ language/pointer.rb
@@ -4,11 +4,17 @@ require_relative '../point'
 
 module Language
   class Pointer
-    attr_accessor :position, :delta
+    attr_reader :grid_bounds, :position
+    attr_accessor :delta
 
-    def initialize(position, delta = Point[1, 0])
+    def initialize(grid_bounds, position, delta = Point[1, 0])
+      @grid_bounds = grid_bounds
       @position = position
       @delta = delta
+    end
+
+    def position=(value)
+      self.position = value % grid_bounds
     end
   end
 end
--- point.rb
+++ point.rb
@@ -3,6 +3,7 @@
 Point = Data.define(:x, :y) do
   def +(other) = Point.new(x + other.x, y + other.y)
   def -(other) = Point.new(x - other.x, y - other.y)
+  def %(other) = Point.new(x % other.x, y % other.y)
   def +@ = self
   def -@ = Point.new(-x, -y)
 end
