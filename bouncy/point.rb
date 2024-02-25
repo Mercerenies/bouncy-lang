1b9cb234397336396bf48071df91af8e4281092d
Silvio Mayolo <mercerenies@comcast.net>
2024-02-25 13:26:06 -0500

    Remove dependency on Ruby 3.2 (should work on older versions now)

--- bouncy/point.rb
+++ bouncy/point.rb
@@ -1,12 +1,37 @@
 # frozen_string_literal: true
 
+# rubocop:disable Naming/MethodParameterName
 module Bouncy
-  Point = Data.define(:x, :y) do
+  class Point
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x = x
+      @y = y
+    end
+
+    def self.[](x, y) = new(x, y)
+
+    def ==(other)
+      other.is_a?(Point) and x == other.x and y == other.y
+    end
+
+    alias eql? ==
+
+    def to_s
+      "(#{x}, #{y})"
+    end
+
+    def hash
+      ['Point', x, y].hash
+    end
+
     def +(other) = Point.new(x + other.x, y + other.y)
     def -(other) = Point.new(x - other.x, y - other.y)
     def %(other) = Point.new(x % other.x, y % other.y)
     def +@ = self
     def -@ = Point.new(-x, -y)
+
     def self.north = Point[0, -1]
     def self.northeast = Point[1, -1]
     def self.east = Point[1, 0]
@@ -17,3 +42,4 @@ module Bouncy
     def self.northwest = Point[-1, -1]
   end
 end
+# rubocop:enable Naming/MethodParameterName
